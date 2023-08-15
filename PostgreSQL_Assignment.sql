7002c928addb99d1e49e64b24e1e48961b5bb0e0
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-08-16 01:05:57 +0600

    data insertion in table completed

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -45,3 +45,33 @@ SELECT
   *
 FROM
   enrollment;
+
+  -- Insert data in students
+
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+    ('Alice', 22, 'alice@example.com', 55, 57, NULL),
+    ('Bob', 21, 'bob@example.com', 34, 45, NULL),
+    ('Charlie', 23, 'charlie@example.com', 60, 59, NULL),
+    ('David', 20, 'david@example.com', 40, 49, NULL),
+    ('Eve', 24, 'newemail@example.com', 45, 34, NULL),
+    ('Rahim', 23, 'rahim@gmail.com', 46, 42, NULL);
+
+-- Insert data in courses
+
+INSERT INTO courses (course_name, credits)
+VALUES
+    ('Next.js', 3),
+    ('React.js', 4),
+    ('Databases', 3),
+    ('Prisma', 3);
+
+-- Insert data enrollment
+
+INSERT INTO enrollment (student_id, course_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 2);
+
