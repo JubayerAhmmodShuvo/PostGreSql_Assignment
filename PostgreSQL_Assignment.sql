6711969624d606495e30f24d8b3478141f5e5e0d
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-08-16 00:45:02 +0600

    student table created successfully

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -4,4 +4,13 @@
 CREATE DATABASE UNIVERSITY_DB;
 
 
- 
+ -- students table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(255),
+    age INTEGER,
+    email VARCHAR(255),
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(255)
+);
